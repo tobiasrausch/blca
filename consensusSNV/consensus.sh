55eccfbedd436f185d7873c5a3c0da982dbe6fe8
Tobias Rausch <rauschtobi@gmail.com>
2026-02-06 10:57:58 +0100

    consensus

--- consensusSNV/consensus.sh
+++ consensusSNV/consensus.sh
@@ -23,7 +23,6 @@ do
 		    ## Concat SNVs and InDels for clairS
 		    bcftools concat -a -O b -o ${ID}tumor.raw.clair3.bcf ${SC} ${SI}
 		    bcftools index ${ID}tumor.raw.clair3.bcf
-		    exit;
 		    
 		    ## clairS: min. 5x coverage
 		    bcftools view -f 'PASS,.' -v snps,indels -m 2 -M 2 -i 'sum(AD) > 5' -O b -o ${ID}tumor.filtered.clair3.bcf ${ID}tumor.raw.clair3.bcf chr1 chr2 chr3 chr4 chr5 chr6 chr7 chr8 chr9 chr10 chr11 chr12 chr13 chr14 chr15 chr16 chr17 chr18 chr19 chr20 chr21 chr22 chrX
@@ -37,8 +36,8 @@ do
 		    ## Consensus
 		    bcftools isec -O b -o ${ID}tumor.consensus.bcf -n=2 -w1 ${ID}tumor.filtered.clair3.bcf ${ID}tumor.filtered.ds.bcf
 		    bcftools index ${ID}tumor.consensus.bcf
-		    #rm ${ID}tumor.filtered.ds.bcf ${ID}tumor.filtered.ds.bcf.csi
-		    #rm ${ID}tumor.filtered.clair3.bcf ${ID}tumor.filtered.clair3.bcf.csi
+		    rm ${ID}tumor.filtered.ds.bcf ${ID}tumor.filtered.ds.bcf.csi
+		    rm ${ID}tumor.filtered.clair3.bcf ${ID}tumor.filtered.clair3.bcf.csi
 		fi
 
 		## Filter against short-read illumina
